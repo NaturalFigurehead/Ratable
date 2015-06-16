c1f199dd4cf0c7b864c224f11336f59af436ebf0
Oliver Reznik <oliverreznik@Olivers-MacBook-Air.local>
2015-06-16 12:29:10 -0500

    Initial Commit

---
+++ Rate Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rate Me
+//
+//  Created by Oliver Reznik on 6/16/15.
+//  Copyright (c) 2015 Oliver Reznik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
